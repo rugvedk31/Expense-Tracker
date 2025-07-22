a2259a0547075ba2b31858dce63a4967e856069c
rugvedk31 <rugvedku253@gmail.com>
2025-07-22 13:17:20 +0530

    api for regi and login

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './database/db.js';
 
+import { register } from "./controllers/user.controller.js"; // register controller
+import { login } from "./controllers/user.controller.js"; // register controller
+
 dotenv.config();
 
 const PORT = process.env.PORT || 6000;
@@ -11,18 +14,31 @@ const app = express();
 
 // middileWare
 app.use(express.json());
-app.use(urlencoded({exteded : true}));
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// cors for connection 
-const corsOption = {
-    origin : "http://localhost:5173",     // soon frontend link here....
-    credential : true,
-}
 
+
+app.use(express.json()); // to parse JSON
+app.use(urlencoded({ extended: true })); // to parse URL-encoded data
+app.use(cookieParser());
+
+// CORS setup
+const corsOption = {
+    origin: "http://localhost:5173",
+    credentials: true,
+};
 app.use(cors(corsOption));
+
+
 
 app.listen(PORT, ()=>{
     connectDB();
     console.log(`server is listning on PORT ${PORT}`);
 })
+
+// test route for registeration 
+app.post("/api/register", register);
+
+// test route for login
+app.post("/api/login",login);
