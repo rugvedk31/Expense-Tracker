52f258f22ea4334fea1a22b85947025247ca1411
rugvedk31 <rugvedku253@gmail.com>
2025-07-27 22:31:21 +0530

    all api's working

--- backend/routes/expense.route.js
+++ backend/routes/expense.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { addExpense } from "../controllers/expense.controller.js";
+import { addExpense, getAllExpenses, markAsDoneOrUndone, removeExpense, updateExpense } from "../controllers/expense.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/addExpense").post(isAuthenticated,addExpense); //next
-
+router.route("/getAll").get(isAuthenticated,getAllExpenses);
+router.route("/remove/:id").delete(isAuthenticated,removeExpense);
+router.route("/update/:id").put(isAuthenticated,updateExpense);
+router.route("/:id/done").put(isAuthenticated,markAsDoneOrUndone);
 export default router;
